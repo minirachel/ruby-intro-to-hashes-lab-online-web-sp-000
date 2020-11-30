ff433aaec7399dc95efe8f7c6804215f0dc37b60
Rachel Levinson <rachel.a.levinson@gmail.com>
2020-11-30 21:02:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
 	monopoly = {
     :railroads => {
       :pieces => 4,
-      :names => {}
+      :names => {},
       :rent_in_dollars => {}
   }
 end
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
         :pennsylvania_railroad => {}
         :b_and_o_railroad => {}
         :shortline => {}
-      }
+      },
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 5,
@@ -52,7 +52,7 @@ def monopoly_with_fourth_tier
         :pennsylvania_railroad => {mortgage_value: "$200"}
         :b_and_o_railroad => {mortgage_value: "$400"}
         :shortline => {mortgage_value: "$800"}
-      }
+      },
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 5,
